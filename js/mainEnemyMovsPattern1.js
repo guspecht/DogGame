c0952a98c8453c6c7315c5bc17d1ccc5cb72a7f7
Gustavo Specht <guspecht@hotmail.com>
2022-08-30 19:00:02 +0800

    enemy move pattern 1

--- js/mainEnemyMovsPattern1.js
+++ js/mainEnemyMovsPattern1.js
@@ -3,36 +3,37 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 CANVAS_WITDH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
+const numberOfEnemies = 10;
 const enemyArrays = [];
 
-const enemyImage = new Image();
-enemyImage.src = './img/enemies/enemy1.png';
 let gameFrame = 0;
 
 class Enemy {
     constructor(){
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.speed = Math.random() * 4 - 2;
+        this.image = new Image();
+        this.image.src = './img/enemies/enemy1.png';
+        // this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.frame = 0;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+
         this.flapSpeed = Math.floor(Math.random() * 4 + 2);
     }
     update(){
-        this.x += this.speed;
-        this.y += this.speed;
+        // this.x += this.speed;
+        this.x += Math.random() * 5 -2.5;
+        // this.y += this.speed;
+        this.y += Math.random() * 5 -2.5;
         if(gameFrame % this.flapSpeed === 0){
             this.frame > 4 ? this.frame = 0 : this.frame++;
         }
     }
     draw(){
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight,this.x, this.y, this.width, this.height);
-
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight,this.x, this.y, this.width, this.height);
     }
 }
 
